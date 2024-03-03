7d1d624652d01c70bb0d097757057fa195f3e4cf
saidalisaid2021 <143885729+saidalisaid2021@users.noreply.github.com>
2024-03-03 08:59:08 -0500

    Update settings.py

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST' : '127.0.0.1',
         'PORT' : '3306',
         'USER': 'root',
-        'PASSWORD': 'Salam-123!',
+        'PASSWORD': 'admin@littlelemon!',
         'OPTIONS': {
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
